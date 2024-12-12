48f398802c7288fcfb6522514d831f56a08a1b2b
Dymas F. Antunes Ribeiro d. Santos <dymas10felipe20@gmail.com>
2024-12-12 18:53:21 -0300

    test

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}' 
 
-@app.route('/Hello')
-def hello (name):
-    return f"Hello, {escape(name)}!"
+@app.route('/post/<int:post_id>')
+def show_post (post_id):
+    return f"Post, {escape(post_id)}!"
+
+@app.route('/path/<path:subpath>')
+def show_subpath (subpath):
+    return f"Subpath, {escape(subpath)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
