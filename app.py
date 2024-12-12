89dbfa2c0df31ae298ec864cbeb4e4e9cf55d7b5
Dymas F. Antunes Ribeiro d. Santos <dymas10felipe20@gmail.com>
2024-12-12 19:04:31 -0300

    test

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask 
+from flask import url_for 
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return "login"
+
 @app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User {escape(username)}' 
+def profile(username):
+    return f'{username}\'s profile'
 
-@app.route('/post/<int:post_id>')
-def show_post (post_id):
-    return f"Post, {escape(post_id)}!"
-
-@app.route('/path/<path:subpath>')
-def show_subpath (subpath):
-    return f"Subpath, {escape(subpath)}"
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile',username ='John Doe'))
 
 if __name__ == '__main__':
     app.run(debug=True)
